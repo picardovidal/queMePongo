fa0c664c4920fbda0d209126a4875c3cae12b8f6
ricardo.vidal <ricardo.vidal@tipsalud.local>
2020-04-17 22:48:36 -0300

    correcciones

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -2,27 +2,28 @@
 public class Prenda {
 	
 	TipoDePrenda tipo;
-	Categoria categoria;
 	Color color;
 	Color colorSecundario;
 	Material material;
 	String nombre;
 	
-	Prenda(String _nombre, TipoDePrenda _tipo,Categoria _categoria, Color _color,Material _material, Color _colorSecundario) throws Exception{
+	Prenda(String _nombre, TipoDePrenda _tipo, Color _color,Material _material, Color _colorSecundario) {
 		// valido nulls
-		if (_tipo == null) throw new NullPointerException("No se pueden crear prendas sin tipo");
-		if (_categoria == null) throw new NullPointerException("No se pueden crear prendas sin categoría");
-		if (_color == null) throw new NullPointerException("No se pueden crear prendas sin color");
-		if (_material == null) throw new NullPointerException("No se pueden crear prendas sin material");
+		if (_tipo == null) throw new PrendaInvalidaException("No se pueden crear prendas sin tipo");
+		if (_color == null) throw new PrendaInvalidaException("No se pueden crear prendas sin color");
+		if (_material == null) throw new PrendaInvalidaException("No se pueden crear prendas sin material");
 		
-		//valido que el tipo y la categoria del tipo coincidan
-		if (!_tipo.getCategoria().equals(_categoria)) throw new Exception("La categoría no se condice con su tipo");
 		
 		tipo = _tipo;
-		categoria = _categoria;
 		color = _color;
 		material = _material;
 		nombre = _nombre;
 		colorSecundario = _colorSecundario;
 	}
+	
+	Categoria getCategoria() {
+		return tipo.getCategoria();
+	}
 }
+
+
---
+++ src/main/java/PrendaInvalidaException.java
@@ -0,0 +1,14 @@
+public class PrendaInvalidaException extends RuntimeException
+{
+
+	public PrendaInvalidaException(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
